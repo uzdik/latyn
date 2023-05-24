4dcdf55c73a70300c9ec871b0e109c622cbedf47
uzdik <89787851+uzdik@users.noreply.github.com>
2023-05-24 20:23:22 +0900

    Update asgi.py

--- latyn/asgi.py
+++ latyn/asgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 ASGI config for latyn project.
 
@@ -15,21 +15,3 @@ from django.core.asgi import get_asgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'latyn.settings')
 
 application = get_asgi_application()
-=======
-"""
-ASGI config for latyn project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
-"""
-
-import os
-
-from django.core.asgi import get_asgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'latyn.settings')
-
-application = get_asgi_application()
->>>>>>> origin/main
