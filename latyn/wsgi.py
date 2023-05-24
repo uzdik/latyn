e7c1b179b56d1efd7806840588702f60233866e2
uzdik <89787851+uzdik@users.noreply.github.com>
2023-05-24 20:24:01 +0900

    Update wsgi.py

--- latyn/wsgi.py
+++ latyn/wsgi.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 """
 WSGI config for latyn project.
 
@@ -15,21 +15,3 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'latyn.settings')
 
 application = get_wsgi_application()
-=======
-"""
-WSGI config for latyn project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
-from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'latyn.settings')
-
-application = get_wsgi_application()
->>>>>>> origin/main
